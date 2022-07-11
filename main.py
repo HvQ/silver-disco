13c20d29bce2797accdc30854ae4f9eb0a54751b
Ilhami <ilhami.oezen@nexperia.com>
2022-07-11 14:12:38 +0200

    3rd commit

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ print("Hello World") # Prints "Hello World"
 
 # I want to read a txt file
 def readtxt():
-    with open("test.txt", "r") as f:
+    with open("template/test.txt", "r") as f:
         print(f.read())
 
 # call the function readtxt
 readtxt()
 # After reading the txt file, I want to write to it
 def writetxt():
-    with open("test.txt", "w") as f:
+    with open("template/test.txt", "w") as f:
         f.write("Hello World")
 # call the function writetxt
 writetxt()
 
-sdasd
